edaaf481186184f7d9f61d6bc9dffb7af81c6ea0
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2020-04-29 08:46:07 +0200

    Add instructional text in case shutdown fails

--- init.lua
+++ init.lua
@@ -121,6 +121,6 @@ function OnPlayerDied(player_entity)
 	if did_shutdown then
 		GamePrint('Shutdown request successful. Bye!')
 	else
-		GamePrint("Sorry, couldn't shutdown. " .. shutdown_error)
+		GamePrint("Sorry, couldn't shutdown. Instead, please turn off your computer manually. Error:" .. shutdown_error)
 	end
 end
