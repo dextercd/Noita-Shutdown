24f3e3c5401c760d7f4d8b1e9cf3f6a355304440
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2020-05-11 16:27:53 +0200

    Add perk and UI icons

--- shutdown_perk.lua
+++ shutdown_perk.lua
@@ -3,8 +3,8 @@ table.insert(perk_list,
 		id = 'SHUTDOWN',
 		ui_name = 'Shutdown on death',
 		ui_description = 'Your computer will turn off when you lose.',
-		ui_icon = 'data/ui_gfx/perk_icons/vampirism.png',
-		perk_icon = 'data/items_gfx/perks/vampirism.png',
+		ui_icon = 'mods/Noita-Shutdown/ui_icon.png',
+		perk_icon = 'mods/Noita-Shutdown/perk_icon.png',
 		func = function(entity_perk_item, entity_who_picked, item_name)
 			EntityLoad('mods/Noita-Shutdown/warning_text.xml')
 
