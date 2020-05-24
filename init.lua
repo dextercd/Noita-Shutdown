6d6af10fa2e8b1b6e16e5ee657e86c7e65078af7
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2020-05-24 18:17:52 +0200

    Put error and suggestion on seperate lines

--- init.lua
+++ init.lua
@@ -142,7 +142,8 @@ function OnPlayerDied(player_entity)
 		if did_shutdown then
 			GamePrint('Shutdown request successful. Bye!')
 		else
-			GamePrint("Sorry, couldn't shutdown. Instead, please turn off your computer manually. Error: " .. shutdown_error)
+			GamePrint(shutdown_error)
+			GamePrint("Sorry, couldn't shutdown. Instead, please turn off your computer manually. Error: ")
 		end
 	end
 end
