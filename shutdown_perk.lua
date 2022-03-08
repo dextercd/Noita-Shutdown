6ea9731bf3447992d1cda1e9cdb7c758d64406a6
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-03-08 17:54:58 +0100

    Move warning_text entity to files/ dir

--- shutdown_perk.lua
+++ shutdown_perk.lua
@@ -8,7 +8,7 @@ table.insert(perk_list,
         stackable = STACKABLE_NO,
         usable_by_enemies = false,
         func = function(entity_perk_item, entity_who_picked, item_name)
-            EntityLoad('mods/shutdown/warning_text.xml')
+            EntityLoad('mods/shutdown/files/entities/misc/warning_text.xml')
 
             GameAddFlagRun('shutdown_on_death')
             GameAddFlagRun('shutdown_reason_perk')
